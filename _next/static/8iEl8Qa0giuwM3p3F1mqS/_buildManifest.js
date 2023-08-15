e8bd72f83aad7a888b30176d07d8782cf4a39cf5
ymekuria <ymekuria@gmail.com>
2023-08-15 10:47:21 -0700

    Deploy gh-pages

---
+++ _next/static/8iEl8Qa0giuwM3p3F1mqS/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/d92f229295992ef1.css","static/chunks/pages/index-c3d98c341ba19e45.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
